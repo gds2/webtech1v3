b4b7ff38df2f7a65dc717f8a9fefe05d559b4351
gds2 <gdswa@outlook.com>
2017-09-05 22:52:20 +0200

    Servlets netjes ingedeeld

--- src/Servlets/CheckRegister.java
+++ src/Servlets/CheckRegister.java
@@ -1,3 +1,5 @@
+package Servlets;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -5,7 +7,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(name = "/CheckRegister")
+@WebServlet(name = "/Servlets.CheckRegister")
 public class CheckRegister extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
--- src/Servlets/DirectToRegister.java
+++ src/Servlets/DirectToRegister.java
@@ -1,3 +1,5 @@
+package Servlets;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -5,16 +7,13 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(name = "Servlet")
-public class Servlet extends HttpServlet {
+@WebServlet(name = "/Servlets.DirectToRegister")
+public class DirectToRegister extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        response.sendRedirect("registreer.html");
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        response.getWriter().write("Hello world");
-        response.getWriter().write("Hello world");
-        response.getWriter().write("Hello world");
-        response.getWriter().write("Hello world AGAIN");
+        response.sendRedirect("registreer.html");
     }
 }
