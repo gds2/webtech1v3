5c4041181cfb29ecb840c70c8f82aa4721a229b1
gds2 <gdswa@outlook.com>
2017-09-05 15:57:25 +0200

    begin van login233d

--- src/CheckLogin.java
+++ src/CheckLogin.java
@@ -5,7 +5,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(name = "CheckLogin")
+@WebServlet(name = "/CheckLogin")
 public class CheckLogin extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String name = request.getParameter("username");
