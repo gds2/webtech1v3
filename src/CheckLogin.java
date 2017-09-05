9e7148c1de20247d561f7748dc05a77233708d16
gds2 <gdswa@outlook.com>
2017-09-05 14:55:20 +0200

    begin van login2

---
+++ src/CheckLogin.java
@@ -0,0 +1,23 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "CheckLogin")
+public class CheckLogin extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String name = request.getParameter("username");
+        String password = request.getParameter("password");
+        
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.getWriter().write("Hello world");
+        response.getWriter().write("Hello world");
+        response.getWriter().write("Hello world");
+        response.getWriter().write("Hello world AGAIN");
+    }
+}
