5d58496047e4c037b327436128bfafc7b5bfd4c3
Tavet Takesian <indexviross@hotmail.com>
2017-09-05 11:17:38 +0200

    test
    # Conflicts:
    #	.gitignore
    #	.idea/workspace.xml

--- src/Servlet.java
+++ src/Servlet.java
@@ -14,5 +14,6 @@ public class Servlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.getWriter().write("Hello world");
         response.getWriter().write("Hello world");
+        response.getWriter().write("Hello world");
     }
 }
