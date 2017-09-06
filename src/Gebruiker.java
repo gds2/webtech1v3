11bc0eb16312df26e7ce4d46587184356b6217f7
swart\Govert <gdswa@outlook.com>
2017-09-06 16:26:05 +0200

    spul

--- src/Gebruiker.java
+++ src/Gebruiker.java
@@ -1,18 +1,58 @@
-public abstract class Gebruiker {
+public class Gebruiker {
     private String naam;
+    private String gebruikersnaam;
     private String password;
+    private String type;
+
+
+    public Gebruiker(String naam, String gebruikersnaam, String password, String type) {
+        this.naam = naam;
+        this.gebruikersnaam = gebruikersnaam;
+        this.password = password;
+        this.type = type;
+    }
+
     /**
      * Controleert of de gebruikersnaam en wachtwoord bij de gebruiker horen
-     * @param naam
+     * @param gebruikersnaam
      * @param password
      * @return
      */
-    public boolean correctLogin(String naam, String password){
-        if(naam.equals(this.naam) && (password.equals(this.password))){
+    public boolean correctLogin(String gebruikersnaam, String password){
+        if(gebruikersnaam.equals(this.gebruikersnaam) && (password.equals(this.password))){
             return true;
         }
         else {
             return false;
         }
     }
+
+    public boolean isHuurder(){
+        if(type.equals("Huurder")){
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    public boolean isVerhuurder(){
+        if(type.equals("Verhuurder")){
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+
+
+    public String getNaam() {
+        return naam;
+    }
+
+    public String getGebruikersnaam() {
+        return gebruikersnaam;
+    }
+
 }
--- src/Kamer.java
+++ src/Kamer.java
@@ -2,11 +2,14 @@ public class Kamer {
    private double grootte;
    private double maxprijs;
    private String plaats;
+   private Gebruiker huurder;
+   private Gebruiker verhuurder;
 
-    public Kamer(double grootte, double maxprijs, String plaats) {
+    public Kamer(double grootte, double maxprijs, String plaats, Gebruiker verhuurder) {
         this.grootte = grootte;
         this.maxprijs = maxprijs;
         this.plaats = plaats;
+        this.verhuurder = verhuurder;
     }
 
     public double getGrootte() {
@@ -19,5 +22,12 @@ public class Kamer {
 
     public String getPlaats() {
         return plaats;
+    }
+
+    public String getHuurderNaam() {
+        return huurder.getNaam();
+    }
+    public void setHuurder(Gebruiker huurder) {
+        this.huurder = huurder;
     }
 }
